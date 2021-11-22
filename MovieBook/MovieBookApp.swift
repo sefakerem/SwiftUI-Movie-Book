00295d57d2977b03da05549fb67de5fbcb76b09a
Sefa Kerem <46871207+sefakerem@users.noreply.github.com>
2021-11-22 18:07:46 +0300

    Initial Commit

---
+++ MovieBook/MovieBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieBookApp.swift
+//  MovieBook
+//
+//  Created by Sefa KEREM on 22.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct MovieBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
